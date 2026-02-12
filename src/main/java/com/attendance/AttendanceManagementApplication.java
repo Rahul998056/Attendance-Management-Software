a10d2b3b0c0741937e842aec2818f23439506e91
Intel <Intel@DESKTOP-0E19SQ4>
2026-02-12 15:57:51 +0530

    chore: commit current attendance management code

--- src/main/java/com/attendance/AttendanceManagementApplication.java
+++ src/main/java/com/attendance/AttendanceManagementApplication.java
@@ -10,4 +10,12 @@ public class AttendanceManagementApplication {
 		SpringApplication.run(AttendanceManagementApplication.class, args);
 	}
 
+	@org.springframework.context.annotation.Bean
+	public org.springframework.boot.CommandLineRunner commandLineRunner(org.springframework.context.ApplicationContext ctx) {
+		return args -> {
+			System.out.println("\n✅=====================================================================");
+			System.out.println("✅ DATABASE CONNECTION SUCCESSFUL! The Application is running on Port 8080");
+			System.out.println("✅=====================================================================\n");
+		};
+	}
 }
